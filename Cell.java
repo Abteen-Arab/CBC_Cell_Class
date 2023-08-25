5f7320f2d41fc9b4ad39bad33f7d946316b879a7
ABTEEN ARAB <abteen.a@gmail.com>
2023-08-25 16:06:09 -0700

    CELL T2

--- Cell.java
+++ Cell.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList; // import just the ArrayList class
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Cell {
 
@@ -14,10 +17,22 @@ public class Cell {
     //Instance Variables (Add-on)
     //--------------------------------------------------
 
-    private boolean cellType;
     private boolean mitotic;
     private int hayFlickLimit;
     private int numDivisions;
+
+    //--------------------------------------------------
+    //Instance Variables: Growth "Factors"
+    //--------------------------------------------------
+    private  HashMap<String,Double> resources = new HashMap<String , Double>();
+    private  HashMap<String,Double> energy = new HashMap<String , Double>();
+
+    //--------------------------------------------------
+    //Signal Molecules
+    //--------------------------------------------------
+
+
+    private HashSet<Boolean> gFac = new HashSet<Boolean>();
 
     public Cell (){
         this(0,0,0,0);
@@ -28,6 +43,13 @@ public class Cell {
         this.x = x;
         this.y = y;
         this.id= id;
+
+        resources.put("Phosphorous", 1.0);
+        resources.put("Nitrogen", 1.0);
+        resources.put("Oxygen", 1.0);
+
+        energy.put("glucose", 1.0);
+        energy.put("ATP", 1.0);
     }
 
     //--------------------------------------------------
@@ -73,19 +95,51 @@ public class Cell {
     //Required Method (not Required)
     //--------------------------------------------------
 
-    public ArrayList<Cell>interactNeighbors (ArrayList<Cell> neighbors) {
+    public ArrayList<Cell> interactNeighbors (ArrayList<Cell> neighbors) {
         return null;
+    }
+
+    //--------------------------------------------------
+    //Energy Methods
+    //--------------------------------------------------
+
+    private double energyCheck() {
+        return energy.get("ATP");
+    }
+
+    private boolean makeEnergy(int numATP) {
+        double ATP = energyCheck();
+        double ox = resources.get("Oxygen");
+        double phos = resources.get("Phosphorous");
+        if (energy.get("glucose")>0.1 && phos - 0.1 *numATP >  0 && ox - 0.1*numATP > 0 && ATP<1-0.1*numATP){
+            energy.replace("ATP", ATP +  0.1*numATP);
+            resources.replace("Phosphorous", phos -  0.1*numATP);
+            resources.replace("Oxygen", phos -  0.1*numATP);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean useEnergy(double enUsed) {
+        double ATP = energyCheck();
+        if (ATP-enUsed>0){
+            energy.replace("ATP", ATP -  enUsed);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     //--------------------------------------------------
     //Division Methods (not Required)
     //--------------------------------------------------
 
-    public boolean setDivState (boolean CT, boolean mitStat, int hayLim, int numDiv){
+    public boolean setDivState (boolean mitStat, int hayLim, int numDiv){
+
         if ((numDiv >= hayLim && mitStat == true) || (numDiv <= hayLim && mitStat == false)) {
             return false;
         } else {
-            this.cellType = CT;
             this.mitotic = mitStat;
             this.hayFlickLimit = hayLim;
             this.numDivisions = numDiv;
@@ -94,8 +148,28 @@ public class Cell {
         return true;
     }
 
-    public void divOff (boolean CT, boolean mitStat, boolean senStat, int hayLim, int numDiv){
-        this.setDivState(false,false,0,0);
+    public void divOff (boolean mitStat, boolean senStat, int hayLim, int numDiv){
+        this.setDivState(false,10,0);
+    }
+
+    public void recognizeSignal(Boolean signal) {
+        gFac.add(signal);
+    }
+
+    public Boolean divideTime (){
+        int gFacPos = 0;
+        if (numDivisions<hayFlickLimit && mitotic && this.energyCheck() > 0.5){
+            for (Boolean i: gFac){
+                if (i) gFacPos++;
+            }
+            if (gFacPos > gFac.size()) {
+                System.out.println("Dividing....");
+                //growth action
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
